a3dee6289f8b4f0e70cc972a3c312c5262c42f38
liucode <1761112388@qq.com>
2018-12-30 13:59:09 +0800

    liu

--- fly/src/components/fly.vue
+++ fly/src/components/fly.vue
@@ -1,35 +1,56 @@
 <template>
-    <div class="fly">
-        <div class="diji" v-for="item in 10" :key>
-            <img :class="'diji' + item" :style="{left:Math.random() * 96 + '%',bottom:djBottom + 'px'}" src="../assets/diji.png" alt="">
+    <div class="fly" @keydown="show($event)">
+        <div class="diji" v-for="item in dj" :key>
+            <img :class="'diji' + item.left" :style="{left:item.left + '%',bottom:djBottom + 'px'}" src="../assets/diji.png" alt="">
         </div>
         <!-- <div v-for="item in 6" :key>
             <img :class="'item' + item" :style="{bottom:zdBottom + 'px',left:zdLeft + item + '%'}" class="bullet" src="../assets/feiji.png" alt="">
         </div> -->
         <img class="bullet" :style="{bottom:zdBottom + 'px',left:zdLeft + '%'}" src="../assets/feiji.png" alt="">
         <img class="aircraft" ref="topInfo" :style="{bottom:numBottom + 'px',left:numLeft + '%'}"  src="../assets/fly.png" alt="" srcset="">
-        <input type="text" autofocus @keydown="show($event)">
+        <input type="text" autofocus >
     </div>
 </template>
 <script>
-export default {
+export default { 
     data(){
         return{
-            numBottom:10,
-            numLeft:50,
-            zdBottom:100,
-            zdLeft:50,
-            djLeft:50,
-            djBottom:900
+            numBottom:10,   //我的飞机距离底边
+            numLeft:50,     //我的飞机距离左边
+            zdBottom:100,   //子弹底边
+            zdLeft:50,      //子弹左边
+            djBottom:900,   //敌机底边
+            dj:[            //敌机数量 敌机距离左边
+                {
+                    left:Math.floor( Math.random() * 100)
+                }
+                // ,{
+                //     left:Math.random() * 100
+                // },{
+                //     left:Math.random() * 100
+                // },{
+                //     left:Math.random() * 100
+                // },{
+                //     left:Math.random() * 100
+                // },{
+                //     left:Math.random() * 100
+                // }
+            ]
         }
     },
     mounted(){
         setInterval(() => {
-            this.zidan()            
+            this.zidan() 
+                      
         }, 30);
+        setInterval(() => {
+            this.diji() 
+        }, 100);
+        this.zidanZdiji()
+
     },
     created(){
-        this.djLeft = Math.random() * 100
+        
     },
     methods:{
         
@@ -37,27 +58,23 @@ export default {
             if(ev.keyCode == 38 && this.numBottom < 500){
                 this.numBottom += 10;
                 console.log(this.numLeft)
-
                 return this.numLeft;
             }
             if(ev.keyCode == 40 && this.numBottom > 0){
                 this.numBottom -= 10;
                 console.log(this.numLeft)
-
                 return this.numLeft;
 
             }
             if(ev.keyCode == 37 && this.numLeft > 2){
                 this.numLeft -= 3;
                 console.log(this.numLeft)
-
                 return this.numLeft;
 
             }
             if(ev.keyCode == 39 && this.numLeft < 96){
                 this.numLeft += 3;
                 console.log(this.numLeft)
-
                 return this.numLeft;
 
             }
@@ -69,6 +86,29 @@ export default {
 
             }else{
                 this.zdBottom = 100
+            }
+        },
+        diji(){
+            if(this.djBottom <= 900 && this.djBottom > 100){
+                this.djBottom -= 10
+            }else{
+                this.djBottom = 900;
+                for (let djnum = 0; djnum < this.dj.length; djnum++) {
+                    this.$set(this.dj[djnum], `left`, Math.floor( Math.random() * 100))
+                }
+            }
+        },
+        zidanZdiji(){
+                console.log(this.djBottom);
+                console.log(this.zdBottom);
+                console.log(this.dj[0].left); 
+                console.log(this.zdLeft);  
+            if(this.djBottom > this.zdBottom && this.dj[0].left + 13 > this.zdLeft && this.dj[0].left - 13 < this.zdLeft){
+                console.log("撞上了");
+                console.log(this.djBottom);
+                console.log(this.zdBottom);
+                console.log(this.dj[0].left); 
+                console.log(this.zdLeft);  
             }
         }
     }
@@ -94,6 +134,7 @@ export default {
 }
 .diji>img{
     width: 50px;
+    margin-left: -25px;
     position: absolute;
 }
 </style>
